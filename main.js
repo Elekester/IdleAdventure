0a538a3392fc62e9e28818a2c391a4dbdd0e0b7b
Elekester <elekester@gmail.com>
2021-01-26 03:25:37 -0600

    Created Idle Adventure

---
+++ main.js
@@ -0,0 +1,25 @@
+var Game = {};
+Game.Version = 0.001;
+
+/** This function returns the DOM element with the given id.
+ * @param {string} str - The name of the id.
+ * @returns {HTMLElement} - The element with the given id.
+ */
+function e(str) {return document.getElementById(str);}
+
+/**
+ * This function removes all child nodes from a given element.
+ * @param {HTMLElement} parent - The element from which to remove all child nodes.
+ */
+function removeChildren(parent) {while (parent.firstChild) {parent.removeChild(parent.firstChild);}}
+
+window.onload = function() {
+	/* Update the topBar div. */
+	e('topBarVersion').innerText = 'v. ' + Game.Version;
+	
+	/* Recreate the game div. */
+	var frag = document.createDocumentFragment();
+	/* Do stuff with the fragment. */
+	removeChildren(e('game'));
+	e('game').appendChild(frag);
+}
