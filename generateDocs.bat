04ab03b3b2532010e77d43cb857f09f92bfb41c2
Elekester <elekester@gmail.com>
2021-01-30 04:21:22 -0600

    Update generateDocs.bat

--- generateDocs.bat
+++ generateDocs.bat
@@ -3,4 +3,5 @@
 rmdir /s /q docs
 :: For some reason better-docs doesn't copy docsStyle.css to the output, so I'll do it myself.
 xcopy docsStyle.css docs\
+:: For some reason, jsdoc exits the batch file, so don't put anything after it.
 jsdoc -c jsdoc.json
