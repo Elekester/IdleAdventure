cdb047bf5f44a73646598b094f44665aa70e5a21
Elekester <elekester@gmail.com>
2021-02-01 17:14:55 -0600

    Move files to docsGen
    Moved docsStyle.css, generateDocs.bat, and jsdoc.json to the docsGen folder.

--- docsGen/generateDocs.bat
+++ docsGen/generateDocs.bat
@@ -1,7 +1,7 @@
 :: The template used is defined in jsdoc.json
 @echo ON
-rmdir /s /q docs
+rmdir /s /q ..\docs
 :: For some reason better-docs doesn't copy docsStyle.css to the output, so I'll do it myself.
-xcopy docsStyle.css docs\
+xcopy docsStyle.css ..\docs\
 :: For some reason, jsdoc exits the batch file, so don't put anything after it.
 jsdoc -c jsdoc.json
