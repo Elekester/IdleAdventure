36588198d734e3134a8e518ef1ec9ee691e59307
Elekester <elekester@gmail.com>
2021-02-01 18:58:56 -0600

    Update generateDocs.bat

--- docsGen/generateDocs.bat
+++ docsGen/generateDocs.bat
@@ -1,6 +1,5 @@
 :: Running this bat file generates the docs folder using jsdoc with the better-docs template.
-:: If you are wanting to generate your own docs, the commands here assume you are using better-docs at the given path and that jsdoc is installed globally. If these aren't true, you'll need to edit this batch file and conf.json appropriately.
-@echo ON
+:: If you are wanting to generate your own docs, the commands here assume you are using better-docs and that it is installed in the same location as jsdoc.
 
 :: Remove the old docs directory.
 rmdir /s /q ..\docs
@@ -8,5 +7,5 @@ rmdir /s /q ..\docs
 :: For some reason better-docs doesn't copy style.css to the output, so we have to do it here.
 xcopy style.css ..\docs\
 
-:: For some reason, jsdoc exits the batch file, so don't put anything after it.
-jsdoc -c conf.json -t "C:/Program Files/nodejs/node_modules/npm/node_modules/better-docs"
+:: For some reason, on my computer jsdoc exits the batch file, so don't put anything after it.
+jsdoc -c conf.json -t ..\better-docs
