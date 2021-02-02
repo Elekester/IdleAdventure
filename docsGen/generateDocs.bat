20a10a34f14314d16ef02b41984c67286dbdb268
Elekester <elekester@gmail.com>
2021-02-01 22:04:26 -0600

    Update docsGen

--- docsGen/generateDocs.bat
+++ docsGen/generateDocs.bat
@@ -1,9 +1,8 @@
-:: Running this bat file generates the docs folder using jsdoc with the better-docs template.
-:: If you are wanting to generate your own docs, the commands here assume you are using better-docs and that it is installed in the same location as jsdoc.
+:: Running this bat file generates the docs folder using jsdoc with the configuration from conf.json.
 @ECHO OFF
 
 :: Remove the old docs directory.
 RD /s /q ..\docs
 
 :: At least on my computer jsdoc exits the batch file, so don't put anything after it.
-JSDOC -c conf.json -t ..\better-docs
+JSDOC -c conf.json
