a8d17b62f866ae06551bcc440fffb3cde252e3e6
Clayton Craig <elekester@gmail.com>
2021-02-04 08:27:58 -0600

    Update generateDocs.bat

--- docsGen/generateDocs.bat
+++ docsGen/generateDocs.bat
@@ -1,8 +1,4 @@
-:: Running this bat file generates the docs folder using JSDoc with the configuration from conf.json.
+:: Running this bat file generates the ../docs folder using JSDoc with the configuration from conf.json.
 @ECHO OFF
-
-:: Remove the old docs directory.
 RD /s /q ..\docs
-
-:: At least on my computer jsdoc exits the batch file, so don't put anything after it.
 JSDOC -c conf.json
